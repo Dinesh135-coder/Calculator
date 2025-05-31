260aa7279dde11cc238b385fc0a40c62409dfd7c
Dinesh135-coder <giri135d06@gmail.com>
2025-05-31 18:17:49 +0545

    Added new function (squre and root )

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Store the current input value
-const DEFAULT_INPUT = "0";
+const DEFAULT_INPUT = "";
 let input = DEFAULT_INPUT;
-
 // Called when a button is clicked (number or operator)
+let previousResult = DEFAULT_INPUT;
+
+function resetInput() {
+  previousResult = document.getElementById("display").innerText ;
+  input = DEFAULT_INPUT;
+}
 function press(key) {
 
   // Add the key to the input string
@@ -14,11 +19,8 @@ function press(key) {
 
 
 // Called when the equals button is clicked
-function calculate(){
-if (input === "") {
-  return;
-}
-// If input is empty, do nothing
+function calculate() {
+  // If input is empty, do nothing
   try {
     // Use eval to calculate the input
     let result = eval(input);
@@ -32,14 +34,56 @@ if (input === "") {
     document.getElementById("display").innerText = "Invalid input";
   } finally {
     // Reset the input to default after calculation
-    input = "";
+    resetInput();
   }
 }
+
+function root() {
+
+  try {
+    console.log("input=" + input);
+    console.log("previousInput=" + previousResult);
+    if (input === DEFAULT_INPUT && previousResult != DEFAULT_INPUT) {
+      input = previousResult;
+    }
+    let value = eval(input); // Use input instead of currentInput
+    console.log("value=" + value);
+    if (value < 0 || value===undefined) {
+      document.getElementById("display").innerText = "Invalid ";
+    } else {
+      let result = Math.sqrt(value);
+      console.log("result=" + result);
+      document.getElementById("display").innerText = result;
+    }
+  } catch {
+    document.getElementById("display").innerText = "Error";
+  } finally {
+    resetInput() // Reset input after calculating
+  }
+}
+
+function square() {
+  try {
+    let value = eval(input); // Use input instead of currentInput
+    let result = value * value;
+    document.getElementById("display").innerText = result;
+  } catch {
+    document.getElementById("display").innerText = "Error";
+  } finally {
+    resetInput(); // Reset input after calculating
+  }
+}
+
 
 // to clear the display
 function clearDisplay() {
   input = DEFAULT_INPUT;
+  previousResult = DEFAULT_INPUT;
   document.getElementById("display").innerText = "0";
 }
 
+function process() { }
 
+// math.sin 
+// complete the squre  function NaN error 
+// Html css for weather app
